a87d95731cd34d3f8b312415cb85b7f733fb14d7
Noissy Robot <95743557+RidwanMusicProject@users.noreply.github.com>
2022-01-10 18:33:23 +0700

    Update stream.py

--- Music/Plugins/stream.py
+++ Music/Plugins/stream.py
@@ -69,8 +69,8 @@ async def vplay(c: Client, message: Message):
     keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{GROUP}"),
-                InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{CHANNEL}"),
+                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/unclesamaja1"),
+                InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/unclesamaja"),
             ],
         ]
     )
