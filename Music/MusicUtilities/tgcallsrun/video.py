c7bc4ee281953723040b86df01a5a38b9aababaa
Noissy Robot <95743557+RidwanMusicProject@users.noreply.github.com>
2022-01-10 17:02:20 +0700

    Update video.py

--- Music/MusicUtilities/tgcallsrun/video.py
+++ Music/MusicUtilities/tgcallsrun/video.py
@@ -24,8 +24,8 @@ from Music.MusicUtilities.tgcallsrun.queues import (
 keyboard = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("ᴅᴏɴᴀsɪ", url=f"https://t.me/{GROUP}"),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{CHANNEL}"),
+            InlineKeyboardButton("ᴅᴏɴᴀsɪ", url=f"https://t.me/{unclesamaja1}"),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{unclesamaja}"),
         ]
     ]
 )
